7cbd83c8a88f3748c1e00e98cba8799eea270c7d
Harshal Gajjar <harshal@Harshals-MacBook-Pro.local>
2018-10-28 16:22:41 +0530

    Update connections/connect.php

--- connections/connect.php
+++ connections/connect.php
@@ -1,14 +1,10 @@
 <?php
 // connects to the DB
 
-<<<<<<< HEAD
    $host        = "host = 172.16.135.141";
-=======
-   $host        = "host = localhost";
->>>>>>> 9f002bb6e765413b4872d465cd588396dc8e4e6b
    $port        = "port = 5432";
    $dbname      = "dbname = project";
-   $credentials = "user = dbuser password=123456";
+   $credentials = "user = postgres password=harshal";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
